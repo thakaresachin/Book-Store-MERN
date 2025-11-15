5f9def8ecd108bd17340656f0272d86af4ee8e4e
sachin thakare <thakaresachin789@gmail.com>
2025-11-15 19:50:32 +0530

    Deleted all files from GitHub repo

--- backend/config/multer.js
+++
@@ -1,9 +0,0 @@
-import multer from "multer";
-
-const storage = multer.diskStorage({
-    destination: "uploads/", filename: (req, file, cb) => {
-        return cb(null, `${Date.now()}-${file.originalname}`);
-    }
-})
-
-export const upload = multer({ storage: storage });
